564722255eabe5cc677fa520ef1b145c4b5fc962
Steven Wilson <steven@dominion79.co.uk>
2019-06-28 10:22:57 +0100

    Simple page conneted to contentful cms

---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import Post from '../components/post'
+
+const client = require('contentful').createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN
+})
+
+function HomePage() {
+  async function fetchContentTypes() {
+    const types = await client.getContentTypes()
+    if (types.items) return types.items
+    console.log('Error getting Content Types.')
+  }
+  async function fetchEntriesForContentType(contentType) {
+    const entries = await client.getEntries({
+      content_type: contentType.sys.id
+    })
+    if (entries.items) return entries.items
+    console.log(`Error getting Entries for ${contentType.name}.`)
+  }
+  const [posts, setPosts] = useState([])
+  useEffect(() => {
+    async function getPosts() {
+      const contentTypes = await fetchContentTypes()
+      const allPosts = await fetchEntriesForContentType(contentTypes[0])
+      setPosts([...allPosts])
+    }
+    getPosts()
+  }, [])
+  return (
+    <>
+      <Head>
+        <title>Next.js + Contentful</title>
+        <link
+          rel="stylesheet"
+          href="https://css.zeit.sh/v1.css"
+          type="text/css"
+        />
+      </Head>
+      {posts.length > 0
+        ? posts.map(p => (
+          console.log(p),
+            <Post
+              date={p.fields.date}
+              key={p.fields.title}
+              image={p.fields.thumbnail.fields.file.url}
+              title={p.fields.title}
+            />
+          ))
+        : null}
+    </>
+  )
+}
+
+export default HomePage
