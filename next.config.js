2224e255006eb6a71efa1baf98140d88e603c285
Steven Wilson <steven@dominion79.co.uk>
2019-07-07 13:47:59 +0100

    Add sass to next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-module.exports = {
-    target: 'serverless',
-    env: {
-      SPACE_ID: process.env.SPACE_ID,
-      ACCESS_TOKEN: process.env.ACCESS_TOKEN
-    }
+const withSass = require('@zeit/next-sass')
+
+module.exports = withSass({
+  cssModules: true,
+  target: 'serverless',
+  env: {
+    SPACE_ID: process.env.SPACE_ID,
+    ACCESS_TOKEN: process.env.ACCESS_TOKEN
   }
+})
+  
