d45d7b2b7a37707eae5903ccf96b4a8d2f0885c5
Steven Wilson <steven@dominion79.co.uk>
2019-07-14 21:03:45 +0100

    Update post title class name

--- components/post.js
+++ components/post.js
@@ -6,7 +6,9 @@ function Post({ date, key, image, title, summary }) {
           <figcaption>{title}</figcaption>
         </figure>
         <header  class="animated">
-          <h4><a href="#" rel="bookmark">{title}</a></h4>
+          <h4 className="heading-4">
+            <a href="#" rel="bookmark">{title}</a>
+          </h4>
           <time datetime="2007-08-29T13:58Z"><i className="fa fa-calendar" aria-hidden="true"></i> {date}</time>
         </header>
         <p>{summary}</p>
