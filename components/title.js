a053e3f0a3aec07fbafe3f1aeda30ce8e77763ff
Steven Wilson <steven@dominion79.co.uk>
2019-07-14 08:09:58 +0100

    Fix title component flex layout

--- components/title.js
+++ components/title.js
@@ -8,7 +8,7 @@ function Title() {
                     <h1 className="heading-1">Steven Alan Wilson</h1>
                 </div>
                 <div className="page-stapline">
-                    <h2 className="heading-2">Full-stack Developer, Designer, Creative Technologist, Founder at <a href="http://www.dominion79.co.uk" target="_blank" title="Vist Dominion79">Dominion79</a>, Foodie, Gamer &amp; Father.</h2>
+                    <h2 className="heading-2">Principal developer at the Ministry of Justice, creative technologist, foodie, gamer, husband &amp; father</h2>
                 </div>
                 <div className="page-current">
                     <p className="medium"><strong>I'm currently at the HMPPS Digital Studio Sheffield</strong> <a href="mailto:hello@stevenalanwilson.com?subject=Website%20enquiry" title="Say Hello">but I'm still always intrested in meeting new people</a></p>
@@ -24,20 +24,22 @@ function Title() {
                 }
                 
                 .page-title {
-                    flex: 1 100%;
+                    flex: 0 0 98%;
+                    margin: 0 1% 2%;
                     padding: ${theme.spacing.basePadding}/2 0;
                     border-bottom: 1px solid ${theme.colours.black}; 
                     
                 }
                 .page-stapline {
-                    flex: 2 73%;
+                    flex: 0 0 72%;
+                    margin: 0 1% 2%;
                     padding: ${theme.spacing.basePadding}/2 0;
                     border-bottom: 3px solid ${theme.colours.black}; 
-                    margin-right: 2%;
                 }
 
                 .page-current {
-                    flex: 2 25%;
+                    flex: 0 0 24%;
+                    margin: 0 1% 2%;
                     padding: ${theme.spacing.basePadding}/2 0;
                     border-bottom: 3px solid ${theme.colours.black}; 
                 }
