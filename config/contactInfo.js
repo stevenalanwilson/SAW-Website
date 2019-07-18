bbd96311a57f42b242e1e3ffea27575538024e9e
Steven Wilson <steven@dominion79.co.uk>
2019-07-18 10:49:08 +0100

    Make the content info dynamic
    - Add content info object and import into the footer component

---
+++ config/contactInfo.js
@@ -0,0 +1,17 @@
+const contactInfo = {
+    number: '07720846954',
+    twitter: [
+        {
+            title: 'Follow me on Twitter',
+            link: 'https://twitter.com/d79design'
+        }
+    ],
+    facebook: [
+        {
+            title: 'Follow me on Facebook',
+            link: 'https://www.facebook.com/stevenalanwilson79/'
+        }
+    ]
+}
+
+export default contactInfo;
