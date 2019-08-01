5e54361066018e7a872d7428c15fb53b1b2af647
Steven Wilson <steven@dominion79.co.uk>
2019-08-01 15:39:20 +0100

    Add Sentry to fetch content type service

--- services/fetchContentType.js
+++ services/fetchContentType.js
@@ -1,10 +1,14 @@
+import * as Sentry from '@sentry/browser';
+
 const contentfulClient = require('../clients/contentfulClient');
 
-async function fetchContentType(contnetType) {
-    const articleContentType = await contentfulClient.getContentType(contnetType)
+Sentry.init({ dsn: "https://844ab33636a24edc9a0c6ce285555d9f@sentry.io/1508421" });
+
+
+async function fetchContentType(contentType) {
+    const articleContentType = await contentfulClient.getContentType(contentType)
     if (articleContentType) return articleContentType
-    console.log('Error getting article content type.')
+    Sentry.captureMessage('Error getting article content type.')
 }
 
-module.exports = fetchContentType
-;
+module.exports = fetchContentType;
