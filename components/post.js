82b489cb3cdf27e3856cab9d41231064bde28a2f
Steven Wilson <steven@dominion79.co.uk>
2019-08-01 15:37:46 +0100

    Fix HTML semantic to post component

--- components/post.js
+++ components/post.js
@@ -1,15 +1,15 @@
-function Post({ date, key, image, title, summary }) {
+function Post({ date, image, title, summary }) {
     return (
       <article className="post content-box borders bottom one">
        <figure>
           <img alt={title} src={image} />
           <figcaption>{title}</figcaption>
         </figure>
-        <header  class="animated">
+        <header>
           <h4 className="heading-4">
             <a href="#" rel="bookmark">{title}</a>
           </h4>
-          <time datetime="2007-08-29T13:58Z"><i className="fa fa-calendar" aria-hidden="true"></i> {date}</time>
+          <time dateTime="2007-08-29T13:58Z"><i className="fa fa-calendar" aria-hidden="true"></i> {date}</time>
         </header>
         <p>{summary}</p>
         <style jsx>{`
