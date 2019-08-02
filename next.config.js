f337ec718f93fda2e2e61714e011819d033f17ac
Steven Wilson <steven@dominion79.co.uk>
2019-08-02 14:05:48 +0100

    Make NextJS taraget config dynaimc
    - An updated to the Next CLI seems to of broken the local development en

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 const withSass = require('@zeit/next-sass')
 
 module.exports = withSass({
-  target: 'serverless',
+  target: process.env.NODE_ENV === "development" ? "server" : "serverless",
   env: {
     SPACE_ID: process.env.SPACE_ID,
     ACCESS_TOKEN: process.env.ACCESS_TOKEN
