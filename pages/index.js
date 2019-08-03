fc400eb07d377a95818f56dc0aa5443a722d08ea
Steven Wilson <steven@dominion79.co.uk>
2019-08-03 20:48:35 +0100

    Add key to posts

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,46 @@
-import { useEffect, useState } from 'react'
-import '../static/screen.scss'
-import Head from 'next/head'
-import Post from '../components/Post'
-import Layout from '../components/Layout.js';
-import fetchContentType from '../services/fetchContentType'
-import fetchEntriesForContentType from '../services/fetchEntriesForContentType'
+import { useEffect, useState } from 'react';
+import '../static/screen.scss';
+
+import Head from 'next/head';
+import Post from '../components/Post';
+import Layout from '../components/Layout';
+
+import fetchContentType from '../services/fetchContentType';
+import fetchEntriesForContentType from '../services/fetchEntriesForContentType';
+
 
 function HomePage() {
 
-  const [posts, setPosts] = useState([])
-  useEffect(() => {
-    async function getPosts() {
-      const contentArticleType = await fetchContentType('article');      
-      const  allPosts = await fetchEntriesForContentType(contentArticleType.sys.id);
-      setPosts([...allPosts])
-    }
-    getPosts()
-  }, [])
+	const [posts, setPosts] = useState([])
+	useEffect(() => {
+		async function getPosts() {
+			const contentArticleType = await fetchContentType('article');
+			const allPosts = await fetchEntriesForContentType(contentArticleType.sys.id);
+			setPosts([...allPosts])
+		}
+		getPosts()
+	}, [])
 
-  return (
-    <>
-      <Head>
-        <title>Steven Alan Wilson</title>
-      </Head>
-      <Layout>
-        {posts.length > 0
-          ? posts.map(p => (
-              <Post
-                date={p.fields.date}
-                key={p.fields.title}
-                image={p.fields.thumbnail.fields.file.url}
-                title={p.fields.title}
-                summary={p.fields.summary}
-              />
-            ))
-      : null}
-      </Layout>
-    </>
-  )
+	return (
+		<>
+			<Head>
+				<title>Steven Alan Wilson</title>
+			</Head>
+			<Layout>
+				{posts.length > 0
+					? posts.map(p => (
+						<Post
+							date={p.fields.date}
+							key={p.sys.id}
+							image={p.fields.thumbnail.fields.file.url}
+							title={p.fields.title}
+							summary={p.fields.summary}
+						/>
+					))
+					: null}
+			</Layout>
+		</>
+	)
 }
 
 export default HomePage
