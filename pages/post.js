0b6d05434ee3d471cb0d7021bfc1b27da6581976
Steven Wilson <steven@dominion79.co.uk>
2019-08-19 20:51:07 +0100

    Add static post page

---
+++ pages/post.js
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react';
+import '../static/screen.scss';
+
+import Head from 'next/head';
+import Layout from '../components/Layout';
+
+function PostPage() {
+	return (
+		<>
+			<Head>
+				<title>Steven Alan Wilson</title>
+			</Head>
+			<Layout>
+				
+			</Layout>
+		</>
+	)
+}
+
+export default PostPage
