a33915ed4ac608859644dd0f70acabb64c22e188
Steven Wilson <steven@dominion79.co.uk>
2019-08-19 21:58:35 +0100

    Add getInitalProps & fetchEntity service to Post

--- pages/post.js
+++ pages/post.js
@@ -1,20 +1,32 @@
-import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+
 import '../static/screen.scss';
 
 import Head from 'next/head';
 import Layout from '../components/Layout';
 
-function PostPage() {
+import fetchEntity from '../services/fetchEntry';
+
+const PostPage = props => {
+	const router = useRouter();
 	return (
 		<>
 			<Head>
-				<title>Steven Alan Wilson</title>
+				<title>Steven Alan Wilson {props.content.title}</title>
 			</Head>
 			<Layout>
+				<h1>{props.content.title}</h1>
 				
 			</Layout>
 		</>
 	)
 }
 
+PostPage.getInitialProps = async (context) => {
+	const { id } = context.query;
+	const entity = await fetchEntity(id);
+	return {
+		content: entity.fields
+	};
+}
 export default PostPage
