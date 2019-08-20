496fc220ec57a8144f362806130d18c02c5ef408
Steven Wilson <steven@dominion79.co.uk>
2019-08-20 14:40:30 +0100

    Refactor post page into two columns

--- pages/post.js
+++ pages/post.js
@@ -1,4 +1,5 @@
 import '../static/screen.scss';
+import theme from '../styles/theme'
 
 import Head from 'next/head';
 import Layout from '../components/Layout';
@@ -12,8 +13,25 @@ const PostPage = props => {
 				<title>Steven Alan Wilson {props.content.title}</title>
 			</Head>
 			<Layout>
-				<h1>{props.content.title}</h1>
+				<div className="main">
+					<h1>{props.content.title}</h1>
+				</div>
+				<div className="sidebar">
+					<p>sidebar</p>
+				</div>				
 			</Layout>
+			<style jsx>{`
+                .main {
+					flex: 0 0 72%;
+                    margin: 0 1% 2%;
+                    padding: ${theme.spacing.basePadding}/2 0;
+				}
+				.sidebar {
+					flex: 0 0 24%;
+                    margin: 0 1% 2%;
+                    padding: ${theme.spacing.basePadding}/2 0;
+				}
+            `}</style>
 		</>
 	)
 }
