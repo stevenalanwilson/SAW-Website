8b4e000c0cd15a3130c170b033e6f208e399f8a5
Steven Wilson <steven@dominion79.co.uk>
2019-09-12 09:56:29 +0100

    Update styling for title.js

--- components/title.js
+++ components/title.js
@@ -1,20 +1,20 @@
 import theme from '../styles/theme'
 
-function Title() {
-    return (
-        <section className="saw-title">
-            <div className="container">
-                <div className="page-title borders bottom one">
-                    <h1 className="heading-1">Steven Alan Wilson</h1>
-                </div>
-                <div className="page-stapline">
-                    <h2 className="heading-2">Principal developer at the Ministry of Justice, creative technologist, foodie, gamer, husband &amp; father</h2>
-                </div>
-                <div className="page-current">
-                    <p className="medium"><strong>I'm currently at the HMPPS Digital Studio Sheffield</strong> <a href="mailto:hello@stevenalanwilson.com?subject=Website%20enquiry" title="Say Hello">but I'm still always intrested in meeting new people</a></p>
-                </div>
-            </div>
-            <style jsx>{`
+function Title () {
+  return (
+    <section className='saw-title'>
+      <div className='container'>
+        <div className='page-title borders bottom one'>
+          <h1 className='heading-1'>Steven Alan Wilson</h1>
+        </div>
+        <div className='page-stapline'>
+          <h2 className='heading-2'>Principal developer at the Ministry of Justice, creative technologist, foodie, gamer, husband &amp; father</h2>
+        </div>
+        <div className='page-current'>
+          <p className='medium'><strong>I'm currently at the HMPPS Digital Studio Sheffield</strong> <a href='mailto:hello@stevenalanwilson.com?subject=Website%20enquiry' title='Say Hello'>but I'm still always intrested in meeting new people</a></p>
+        </div>
+      </div>
+      <style jsx>{`
                 .container {
                     width: 1200px;
                     margin: 0 auto;
@@ -44,9 +44,10 @@ function Title() {
                     border-bottom: 3px solid ${theme.colours.black}; 
                 }
             
-            `}</style>
-        </section>
-    )
+            `}
+      </style>
+    </section>
+  )
 }
 
 export default Title
