9000b0bb6f2e108846a2c8a6bafbaa7ca640fb17
Steven Wilson <steven@dominion79.co.uk>
2020-01-04 12:29:16 +0000

    Update styles to make siblings the same height
    - Flex to make all siblings the same height

--- components/title.js
+++ components/title.js
@@ -6,13 +6,17 @@ function Title () {
           <h1 className='heading-1 font-bold text-6xl'>Steven Alan Wilson</h1>
         </div>
       </div>
-      <div className='flex flex-wrap'>
-        <div className='w-full md:w-3/4 border-black border-b-4 pb-6 my-6'>
-          <h2 className='heading-2 leading-tight text-4xl mr-40'>Principal developer at the Ministry of Justice, creative technologist, foodie, gamer, husband &amp; father</h2>
+      <div className='flex'>
+        <div className='flex flex-wrap w-full md:w-3/4'>
+          <div className='border-black border-b-4 pb-6 my-6 mx-4 flex'>
+            <h2 className='heading-2 leading-tight text-4xl mr-40'>Principal developer at the Ministry of Justice, creative technologist, foodie, gamer, husband &amp; father</h2>
+          </div>
         </div>
 
-        <div className='w-full md:w-1/4 border-black border-b-4 pb-6 my-6'>
-          <p className='text-xl'><strong>I'm currently at the HMPPS Digital Studio Sheffield</strong> <a href='mailto:hello@stevenalanwilson.com?subject=Website%20enquiry' title='Say Hello'>but I'm still always intrested in meeting new people</a></p>
+        <div className='flex flex-wrap w-full md:w-1/4'>
+          <div className='border-black border-b-4 pb-6 my-6 mx-4 flex'>
+            <p className='text-xl'><strong>I'm currently at the HMPPS Digital Studio Sheffield</strong> <a href='mailto:hello@stevenalanwilson.com?subject=Website%20enquiry' title='Say Hello'>but I'm still always intrested in meeting new people</a></p>
+          </div>
         </div>
       </div>
     </section>
