e180e10e4b85702078eadd24bda878058028b3bb
Steven Wilson <steven@dominion79.co.uk>
2020-01-05 20:54:59 +0000

    Update Post component styling
    - Add extra div to add bottom border
    - Update spacing to match design

--- components/post.js
+++ components/post.js
@@ -4,24 +4,26 @@ import { faCalendarAlt } from '@fortawesome/free-solid-svg-icons'
 
 function Post ({ date, id, image, title, summary }) {
   return (
-    <article className='w-full md:w-2/4 p-2 mb-4'>
-      <figure>
-        <Link href={`/post?id=${id}`}>
-          <a>
-            <img alt={title} src={image} className='mb-2' />
-          </a>
-        </Link>
-        <figcaption className='italic text-gray-500 mb-2'>{title}</figcaption>
-      </figure>
-      <header>
-        <h4 className='text-2xl heading-4  mb-2'>
+    <article className='w-full md:w-2/4'>
+      <div className='py-2 mx-4 mb-4 border-black border-b'>
+        <figure>
           <Link href={`/post?id=${id}`}>
-            <a className='underline'>{title}</a>
+            <a>
+              <img alt={title} src={image} className='mb-2' />
+            </a>
           </Link>
-        </h4>
-        <time dateTime='2007-08-29T13:58Z' className='block mb-2'><FontAwesomeIcon icon={faCalendarAlt} /> {date}</time>
-      </header>
-      <p className='leading-tight mb-2'>{summary}</p>
+          <figcaption className='italic text-gray-500 mb-2'>{title}</figcaption>
+        </figure>
+        <header>
+          <h4 className='text-2xl heading-4  mb-2'>
+            <Link href={`/post?id=${id}`}>
+              <a className='underline'>{title}</a>
+            </Link>
+          </h4>
+          <time dateTime='2007-08-29T13:58Z' className='block mb-2'><FontAwesomeIcon icon={faCalendarAlt} /> {date}</time>
+        </header>
+        <p className='leading-tight mb-2'>{summary}</p>
+      </div>
     </article>
   )
 }
