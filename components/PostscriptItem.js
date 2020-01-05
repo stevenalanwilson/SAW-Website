7f658cf9171b569cecfd60f0b91bee1844c1650a
Steven Wilson <steven@dominion79.co.uk>
2020-01-05 20:58:35 +0000

    Update Postscriptitem component to match design

--- components/PostscriptItem.js
+++ components/PostscriptItem.js
@@ -4,7 +4,7 @@ function PostscriptItem ({ title, image, caption, description }) {
   return (
     <div className='w-full md:w-1/4 p-2 my-6 postscript-item'>
       <h3 className='heading-3 text-4xl border-b-2 text-white pb-2 mb-6'>{title}</h3>
-      <figure className='border-b-2 text-white pb-2 mb-4'>
+      <figure className='border-b text-white pb-2 mb-4'>
         <img alt={title} src={image} className='my-4' />
         <figcaption className='italic text-gray-500 mb-2'>{title}</figcaption>
       </figure>
