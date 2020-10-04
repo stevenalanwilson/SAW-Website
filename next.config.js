dbfeb50e7dda7f352bcb82ceb46a1c7d578e259c
Steven Wilson <steven@dominion79.co.uk>
2020-10-04 09:59:14 +0100

    Add basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
+  basePath: '/docs',
   target: process.env.NODE_ENV === 'development' ? 'server' : 'serverless',
   env: {
     SPACEID: process.env.SPACEID,
