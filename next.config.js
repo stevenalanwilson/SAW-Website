2e12fe2f0502f879578249f4e997175d27b3dbc9
Steven Wilson <steven@dominion79.co.uk>
2020-10-04 10:05:28 +0100

    Revert next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-  basePath: '',
+  target: process.env.NODE_ENV === 'development' ? 'server' : 'serverless',
   env: {
     SPACEID: process.env.SPACEID,
     ACCESSTOKEN: process.env.ACCESSTOKEN,
