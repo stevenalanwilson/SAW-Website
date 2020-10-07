391377430c9287a6571f74f2fdb633df43583efe
Steven Wilson <steven@dominion79.co.uk>
2020-10-07 07:09:39 +0100

    Updating case on post and about pages

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Layout from '../components/Layout.js'
+
+function about () {
+  return (
+    <>
+      <Head>
+        <title>About</title>
+      </Head>
+      <Layout>
+        <p>About</p>
+      </Layout>
+    </>
+  )
+}
+
+export default about
