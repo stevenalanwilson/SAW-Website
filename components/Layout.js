e7184b23e2ac38b36500f6d41b5bca2f930292d2
Steven Wilson <steven@dominion79.co.uk>
2020-12-21 20:39:11 +0000

    Remove font awsome styles

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,6 @@ const Layout = props => (
         href='https://fonts.googleapis.com/css?family=Roboto|Roboto+Mono'
         type='text/css'
       />
-      <script src='https://kit.fontawesome.com/f1741a1610.js' crossOrigin='anonymous' />
     </Head>
     <SiteHeader />
     <main>
