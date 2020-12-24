ad121775595798b3dd144aebf9317b90d6fb381b
Steven Wilson <steven@dominion79.co.uk>
2020-12-24 08:57:53 +0000

    Refactor tablet styles

--- components/Footer.js
+++ components/Footer.js
@@ -11,9 +11,9 @@ function Footer () {
     <footer className='bg-gray-900 footer'>
       <div className='container  mx-auto'>
         <div className='flex flex-wrap'>
-          <div className='flex w-full md:w-1/4 p-2'>
-            <div className='border-white hidden md:block w-full border-b-2 mx-4 py-6'>
-              <h3 className='heading-3  text-4xl border-b-2 text-white pb-4 mb-4'>Sitemap</h3>
+          <div className='flex hidden lg:block w-full md:w-1/2 lg:w-1/4 p-2'>
+            <div className='border-white w-full border-b-2 mx-4 py-6'>
+              <h3 className='heading-3  text-2xl lg:text-4xl border-b-2 text-white pb-4 mb-4'>Sitemap</h3>
               <ul className='menu'>
                 <li className='mb-4 pb-4 border-b'>
                   <Link href='/'>
@@ -33,9 +33,9 @@ function Footer () {
               </ul>
             </div>
           </div>
-          <div className='flex w-full md:w-1/4 p-2'>
+          <div className='flex w-full md:w-1/2 lg:w-1/4 p-2'>
             <div className='p-2 border-white w-full border-b-2 mx-4 py-6'>
-              <h3 className='heading-3 text-4xl border-b-2 text-white pb-4 mb-4'>Contact Info</h3>
+              <h3 className='heading-3 text-2xl lg:text-4xl border-b-2 text-white pb-4 mb-4'>Contact Info</h3>
               <ul className='menu'>
                 <li className='mb-4 pb-4 border-b'><p className='text-white'>t. {footerContactInfo.number}</p></li>
                 <li className='mb-4 pb-4 border-b'><p className='text-white'><i className='fa fa-facebook-official' aria-hidden='true' /> <a href={footerContactInfo.twitter[0].link} title={footerContactInfo.twitter[0].title}>{footerContactInfo.twitter[0].title}</a></p></li>
@@ -43,9 +43,9 @@ function Footer () {
               </ul>
             </div>
           </div>
-          <div className='flex w-full md:w-2/4 p-2'>
+          <div className='flex w-full md:w-1/2 lg:w-2/4 p-2'>
             <div className='border-white w-full border-b-2 mx-4 py-6'>
-              <h3 className='heading-3 text-4xl border-b-2 text-white pb-4 mb-4'>About Steven</h3>
+              <h3 className='heading-3 text-2xl lg:text-4xl border-b-2 text-white pb-4 mb-4'>About Steven</h3>
               <p className='text-white leading-loose'>Hello, my name is Steven Wilson and I am a freelance full-stack digital developer and designer based in Derby UK. I’ve been in the industry for over 10 years and have had the pleasure of working with some of the UK's top creative agencies and also the UK's biggest brands such as Speedo International and Sainsburys.</p>
               <p className='text-white leading-loose'>I have a passion for UI design, learning new technologies and experimenting with mixing the digital world with the physical world. In my spare time I like to cook for family and friends, game on my Xbox and spend time with my family.</p>
             </div>
@@ -53,7 +53,7 @@ function Footer () {
         </div>
         <div className='flex flex-wrap'>
           <div className='w-full quote border-white border-b-2 mx-4 py-6'>
-            <h4 className='text-center  text-2xl text-white'>{randomQuote}</h4>
+            <h4 className='text-center text-2xl text-white'>{randomQuote}</h4>
           </div>
         </div>
         <div className='flex flex-wrap'>
--- components/PostscriptItem.js
+++ components/PostscriptItem.js
@@ -3,14 +3,14 @@ import theme from '../styles/theme'
 function PostscriptItem ({ title, image, caption, description }) {
   return (
     <div className='w-full md:w-1/4 p-2 my-6 postscript-item'>
-      <h3 className='heading-3 text-4xl border-b-2 text-white pb-2 mb-6'>{title}</h3>
+      <h3 className='heading-3 text-2xl lg:text-4xl border-b-2 text-white pb-2 mb-6'>{title}</h3>
       <figure className='border-b text-white pb-2 mb-4'>
-      <picture>
-        <source media='(min-width: 800px)' srcset={image + '?fit=thumb&w=800&h=600&fm=webp&q=80'} />
-        <source media='(min-width: 400px)' srcset={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} />
-        <img src={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} alt='Source sets are awesome!' className='my-4' />
-      </picture>
-      <figcaption className='italic text-gray-500 mb-2'>{title}</figcaption>
+        <picture>
+          <source media='(min-width: 800px)' srcset={image + '?fit=thumb&w=800&h=600&fm=webp&q=80'} />
+          <source media='(min-width: 400px)' srcset={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} />
+          <img src={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} alt='Source sets are awesome!' className='my-4' />
+        </picture>
+        <figcaption className='italic text-gray-500 mb-2'>{title}</figcaption>
       </figure>
       <p className='text-white leading-tight'>{description}</p>
     </div>
