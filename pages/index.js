b17d8ad4e4e88201c39efb539ac12db62aa0c735
Steven Wilson <steven@dominion79.co.uk>
2024-11-08 13:36:18 +0000

    Refactor Styling
    - Moved post listing into main col

--- pages/index.js
+++ pages/index.js
@@ -8,8 +8,8 @@ import ListPosts from "../components/listposts"
 
 export async function getStaticProps() {
   const posts = await postsController.getAllPosts()
-  return { 
-    props: { posts } 
+  return {
+    props: { posts }
   }
 };
 
@@ -20,27 +20,39 @@ const index = props => {
         <title>Steven Alan Wilson Limited - Technical Leadership Consultancy</title>
       </Head>
       <Layout>
-        
-      <header>
-        <div className='container mx-auto'>
-          <SiteTitle />
-        </div>
-      </header>
 
-      <main>
-        <div className='container mx-auto'>
-          <ListPosts posts={props.posts}/>
-        </div>
-      </main>
-
-      <footer className='postscript bg-gray-800'>
-      <div className='container mx-auto'>
-        <div className='flex flex-wrap'>
+        <header>
+          <div className='container mx-auto'>
+            <SiteTitle />
           </div>
-        </div>
-      </footer>
+        </header>
+
+        <main>
+          <div className='container mx-auto'>
+            <div className='flex'>
+              <div className='flex flex-wrap w-full md:w-3/4'>
+                <div className=''>
+                <ListPosts posts={props.posts} />
+                </div>
+              </div>
+
+              <div className='flex flex-wrap w-full md:w-1/4'>
+                <div className=''>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </main>
+
+        <footer className='postscript bg-gray-800'>
+          <div className='container mx-auto'>
+            <div className='flex flex-wrap'>
+            </div>
+          </div>
+        </footer>
       </Layout>
-      
+
     </>
   )
 }
