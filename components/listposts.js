a2db00e6b47e7155d6def9554bcc6795749656b2
Steven Wilson <steven@dominion79.co.uk>
2024-11-08 15:02:02 +0000

    Reactored design to a single listing

--- components/listposts.js
+++ components/listposts.js
@@ -3,21 +3,21 @@ import Link from 'next/link';
 
 export default function listposts({ posts }) {
   return (
-    <div className='grid grid-cols-2 gap-1'>
+    <div className='grid grid-cols-1 gap-1'>
       {
         posts.map(({ postSlug, postMetaData }) => (
           <div key={postSlug} className='border-black border-b border-t lg:border-t-0 p-4 mb-5'>
             <Link href={`/post/${postSlug}`} >
-              <h1 className='text-3xl font-bold heading-4 mb-5 underline decoration-3 underline-offset-8 hover:no-underline'>{postMetaData.title}</h1>
+              <h1 className='text-6xl font-bold heading-4 mb-10 underline decoration-3 underline-offset-8 hover:no-underline  w-2/3'>{postMetaData.title}</h1>
               <Image
-                width={900}
-                height={340}
+                width={1000}
+                height={240}
                 alt={postMetaData.title}
                 src={postMetaData.thumbnail}
-                className='mb-5'
+                className='mb-5 w-full' 
               />
             </Link>
-            <p className='text-xl mb-2'>{postMetaData.summary} <Link href={`/post/${postSlug}`} className='underline decoration-3 underline-offset-8 hover:no-underline'>... read more</Link></p>
+            <p className='text-xl mb-2 w-5/6 leading-relaxed'>{postMetaData.summary} <Link href={`/post/${postSlug}`} className='underline decoration-3 underline-offset-8 hover:no-underline'>... read more</Link></p>
           </div>
         ))
       }
