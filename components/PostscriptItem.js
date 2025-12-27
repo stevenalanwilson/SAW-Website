82e92bbdc431afadc53b6a2701647ba54ebf4e87
Steven Wilson <steven@dominion79.co.uk>
2025-12-27 12:24:17 +0000

    fixed the srcset/srcSet warning in PostscriptItem.
    What Was Fixed:
    File: components/PostscriptItem.js (lines 10-11)
    
    Change: Updated the <source> elements to use React's camelCase prop naming:
    
    ❌ Before: srcset={...} (HTML attribute name)
    ✅ After: srcSet={...} (React prop name)
    Why This Matters:
    React uses camelCase for DOM properties to stay consistent with JavaScript naming conventions. Using the incorrect casing causes:
    
    Console warnings during tests and development
    Potential issues with React's property handling
    Violation of React best practices

--- components/PostscriptItem.js
+++ components/PostscriptItem.js
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types'
 import theme from '../styles/theme'
 
-function PostscriptItem ({ title, image, caption, description }) {
+function PostscriptItem({ title, image, caption, description }) {
   return (
     <div className='w-full md:w-1/4 p-2 my-6 postscript-item'>
       <h1 className='heading-3 text-2xl lg:text-4xl border-b-2 text-white pb-2 mb-6'>{title}</h1>
       <figure className='border-b text-white pb-2 mb-4'>
         <picture>
-          <source media='(min-width: 800px)' srcset={image + '?fit=thumb&w=800&h=600&fm=webp&q=80'} />
-          <source media='(min-width: 400px)' srcset={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} />
-          <img src={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'} alt='Source sets are awesome!' className='my-4' />
+          <source
+            media='(min-width: 800px)'
+            srcSet={image + '?fit=thumb&w=800&h=600&fm=webp&q=80'}
+          />
+          <source
+            media='(min-width: 400px)'
+            srcSet={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'}
+          />
+          <img
+            src={image + '?fit=thumb&w=400&h=300&fm=webp&q=80'}
+            alt='Source sets are awesome!'
+            className='my-4'
+          />
         </picture>
         <figcaption className='italic text-xs	text-gray-200 mb-2'>{title}</figcaption>
       </figure>
@@ -22,7 +32,7 @@ PostscriptItem.propTypes = {
   title: PropTypes.string.isRequired,
   image: PropTypes.string.isRequired,
   caption: PropTypes.string,
-  description: PropTypes.string.isRequired
+  description: PropTypes.string.isRequired,
 }
 
 export default PostscriptItem
