399fea7b84c9781cdf7c4523d623e05aa32a2ee8
Steven Wilson <steven@dominion79.co.uk>
2025-12-28 11:40:23 +0000

    Cleanup: Remove unused theme.js and migrate from SCSS to CSS
    - Remove unused styles/theme.js (legacy file)
    - Convert screen.scss to screen.css (only contained Tailwind imports)
    - Remove sass dependency from package.json
    - All tests passing (256/256)
    - Build successful
    - Custom theme system preserved

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import ErrorBoundary from '../components/ErrorBoundary'
 
-import '../public/static/screen.scss'
+import '../public/static/screen.css'
 
 const LANG = 'en'
 
