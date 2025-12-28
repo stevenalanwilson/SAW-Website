a0177c09ca0ba07368be2388a6a454b7bfd5dc46
Steven Wilson <steven@dominion79.co.uk>
2025-12-28 21:36:55 +0000

    Fixed: dotenv Import Error

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
-require('dotenv').config()
-
 const { withSentryConfig } = require('@sentry/nextjs')
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
