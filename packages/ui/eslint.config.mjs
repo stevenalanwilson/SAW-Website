2115ad7a372145e4f6127eff347062e6605bdb3c
Steven Wilson <steven@dominion79.co.uk>
2025-12-30 12:01:42 +0000

    Build fixes

--- packages/ui/eslint.config.mjs
+++ packages/ui/eslint.config.mjs
@@ -1,3 +1,16 @@
 import { createEslintConfig } from '../../packages/config/eslint.config.mjs'
 
-export default createEslintConfig('./tsconfig.json')
+const uiConfig = createEslintConfig('./tsconfig.eslint.json')
+
+// Add ignore pattern for generated files
+export default [
+  {
+    ignores: [
+      '**/*.d.ts',
+      '**/*.js',
+      '!eslint.config.mjs',
+      '!jest.config.js',
+    ],
+  },
+  ...uiConfig,
+]
